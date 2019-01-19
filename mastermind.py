a1e154a7d02a84dfa5d3a99b50334d67817a8420
Maxim Stykow <46091730+mstykow@users.noreply.github.com>
2019-01-18 16:35:05 -0800

    Add files via upload

---
+++ mastermind.py
@@ -0,0 +1,42 @@
+#! python3
+# Mastermind Game for Python
+
+import random
+
+def print_guesses(list):
+    print('---' + 'GUESS'.center(9, '-') + 'CONTAINED'.center(9) + 'EXACT'.center(9, '-') + '---')
+    for item in list:
+        print(str(list.index(item) + 1) + '. ' + ''.join(item[0:5]).center(9) + str(item[5]).center(9) + str(item[6]).center(9))
+
+codeletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+code = random.sample(codeletters, 5)
+guesslist = []
+
+print('Try to guess the correct 5-letter code out of A, B, C, D, E, F, G, H.')
+print('Each letter occurs only once. You have 8 attempts.')
+
+for i in range(8):
+    guesslist.append(list(input('Please enter a guess: ')))
+    if guesslist[i] == code:
+        print('You cracked the code!')
+        break
+    elif guesslist[i] != code and i == 7:
+        print('You ran out of guesses.')
+        print('The correct answer was ' + ''.join(code))
+
+    match1=0
+    for guess_letter in guesslist[i]:
+        if guess_letter in code:
+            match1 += 1
+    
+    match2=0
+    for j in range(len(code)):
+        if guesslist[i][j] == code[j]:
+            match2 += 1
+
+    guesslist[i].extend([match1, match2])
+
+    print_guesses(guesslist)
+
+# for guess in guesslist:
+#        print(''.join(guess) + ' contained: ' + str(match1) + ' exact: ' + str(match2))
